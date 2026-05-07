f011f00fcaaba05c6c30e74982f8b580275493be
LowieCasteels <lowie.casteels@gmail.com>
2026-05-07 14:20:43 +0200

    ezae

--- server.js
+++ server.js
@@ -123,7 +123,9 @@ app.post("/api/chat", (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+}
+
+export default app;
