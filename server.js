424c3808adb2c0eea441d348e287d3213f2f3768
LowieCasteels <lowie.casteels@gmail.com>
2026-05-07 14:17:18 +0200

    eraz

--- server.js
+++ server.js
@@ -123,5 +123,7 @@ app.post("/api/chat", (req, res) => {
   }
 });
 
-
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
